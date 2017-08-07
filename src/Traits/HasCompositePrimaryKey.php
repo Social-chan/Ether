0f0e603adaa8cf050d43b24b4cc807f9905b3099
d8vjork <d8vjork@outlook.com>
2017-08-07 13:44:10 +0000

    Apply fixes from StyleCI

--- src/Traits/HasCompositePrimaryKey.php
+++ src/Traits/HasCompositePrimaryKey.php
@@ -4,45 +4,46 @@ namespace Socialchan\Ether\Traits;
 
 use Illuminate\Database\Eloquent\Builder;
 
-trait HasCompositePrimaryKey {
-	/**
-	 * Set the keys for a save update query.
-	 *
-	 * @param  \Illuminate\Database\Eloquent\Builder  $query
-	 * 
-	 * @return \Illuminate\Database\Eloquent\Builder
-	 */
-	protected function setKeysForSaveQuery(Builder $query)
-	{
-		$keys = $this->getKeyName();
-		if(!is_array($keys)){
-			return parent::setKeysForSaveQuery($query);
-		}
+trait HasCompositePrimaryKey
+{
+    /**
+     * Set the keys for a save update query.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    protected function setKeysForSaveQuery(Builder $query)
+    {
+        $keys = $this->getKeyName();
+        if (!is_array($keys)) {
+            return parent::setKeysForSaveQuery($query);
+        }
 
-		foreach($keys as $keyName){
-			$query->where($keyName, '=', $this->getKeyForSaveQuery($keyName));
-		}
+        foreach ($keys as $keyName) {
+            $query->where($keyName, '=', $this->getKeyForSaveQuery($keyName));
+        }
 
-		return $query;
-	}
+        return $query;
+    }
 
-	/**
-	 * Get the primary key value for a save query.
-	 *
-	 * @param mixed $keyName
-	 * 
-	 * @return mixed
-	 */
-	protected function getKeyForSaveQuery($keyName = null)
-	{
-		if(is_null($keyName)){
-			$keyName = $this->getKeyName();
-		}
+    /**
+     * Get the primary key value for a save query.
+     *
+     * @param mixed $keyName
+     *
+     * @return mixed
+     */
+    protected function getKeyForSaveQuery($keyName = null)
+    {
+        if (is_null($keyName)) {
+            $keyName = $this->getKeyName();
+        }
 
-		if (isset($this->original[$keyName])) {
-			return $this->original[$keyName];
-		}
+        if (isset($this->original[$keyName])) {
+            return $this->original[$keyName];
+        }
 
-		return $this->getAttribute($keyName);
-	}
+        return $this->getAttribute($keyName);
+    }
 }
