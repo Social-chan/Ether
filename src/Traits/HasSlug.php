4faad5f95b3f5c53a8398078506bdb2578c8361f
d8vjork <d8vjork@outlook.com>
2017-08-08 08:57:57 +0000

    Apply fixes from StyleCI

--- src/Traits/HasSlug.php
+++ src/Traits/HasSlug.php
@@ -2,31 +2,31 @@
 
 trait HasSlug
 {
-	/**
-	 * Get slug if not get its ID
-	 * 
-	 * @param string $value
-	 * 
-	 * @return string|int
-	 */
-	public function getSlugAttribute($value)
-	{
-		if ($value) {
-			return $value;
-		} else {
-			return $this->id;
-		}
-	}
+    /**
+     * Get slug if not get its ID.
+     *
+     * @param string $value
+     *
+     * @return string|int
+     */
+    public function getSlugAttribute($value)
+    {
+        if ($value) {
+            return $value;
+        } else {
+            return $this->id;
+        }
+    }
 
-	/**
-	 * Set the slug attribute
-	 *
-	 * @param mixed $value
-	 * 
-	 * @return void
-	 */
-	public function setSlugAttribute($value)
-	{
-		$this->attributes['slug'] = str_slug($value, '-');
-	}
+    /**
+     * Set the slug attribute.
+     *
+     * @param mixed $value
+     *
+     * @return void
+     */
+    public function setSlugAttribute($value)
+    {
+        $this->attributes['slug'] = str_slug($value, '-');
+    }
 }
